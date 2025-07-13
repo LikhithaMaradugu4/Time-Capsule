275e22d9d9ad5b10cfb3c288a37c8cf761d92b60
MARADUGU LIKHITHA <maradugulikhitha@gmail.com>
2025-07-13 17:23:32 +0000

    Implemented basic Login and SignUp

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,53 +1,59 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters']
+  },
   email: {
     type: String,
-    required: [true, 'Email is required'],
+    required: [true, 'Please provide an email'],
     unique: true,
     lowercase: true,
-    trim: true,
-    validate: {
-      validator: function(v) {
-        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
-      },
-      message: props => `${props.value} is not a valid email address!`
-    }
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      'Please provide a valid email'
+    ]
   },
   password: {
     type: String,
-    required: [true, 'Password is required'],
-    minlength: [8, 'Password must be at least 8 characters long']
+    required: [true, 'Please provide a password'],
+    minlength: [6, 'Password must be at least 6 characters'],
+    select: false // Don't include password in queries by default
   },
-  name: {
+  role: {
     type: String,
-    trim: true
+    enum: ['user', 'admin'],
+    default: 'user'
   },
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  timeCapsules: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'TimeCapsule'
+  }]
 });
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
+  // Only hash if password is modified
+  if (!this.isModified('password')) {
+    return next();
   }
+  
+  // Hash password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
-// Method to check password validity
+// Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
-
-export default User;
+export default mongoose.model('User', userSchema);
